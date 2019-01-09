e04bc0ded6b5bc4a2183a1517f8dfcf7741fc8c5
JB JB <jb-._@hotmail.com>
2019-01-09 11:25:03 +0100

    Add project files.

---
+++ GameOfLife/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace GameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pView = new System.Windows.Forms.PictureBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.bClear = new System.Windows.Forms.Button();
+            this.lStats = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.hSpeed = new System.Windows.Forms.HScrollBar();
+            this.cRun = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lFps = new System.Windows.Forms.Label();
+            this.FPS = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pView
+            // 
+            this.pView.BackColor = System.Drawing.Color.Black;
+            this.pView.Location = new System.Drawing.Point(0, 39);
+            this.pView.Name = "pView";
+            this.pView.Size = new System.Drawing.Size(950, 550);
+            this.pView.TabIndex = 0;
+            this.pView.TabStop = false;
+            this.pView.Click += new System.EventHandler(this.pView_Click);
+            this.pView.Paint += new System.Windows.Forms.PaintEventHandler(this.pView_Paint);
+            this.pView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pView_MouseDown);
+            this.pView.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pView_MouseMove);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(875, 2);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(36, 31);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "X";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // bClear
+            // 
+            this.bClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bClear.Location = new System.Drawing.Point(135, -1);
+            this.bClear.Name = "bClear";
+            this.bClear.Size = new System.Drawing.Size(80, 20);
+            this.bClear.TabIndex = 2;
+            this.bClear.Text = "Clear";
+            this.bClear.UseVisualStyleBackColor = true;
+            this.bClear.Click += new System.EventHandler(this.bClear_Click);
+            // 
+            // lStats
+            // 
+            this.lStats.AutoSize = true;
+            this.lStats.ForeColor = System.Drawing.Color.Red;
+            this.lStats.Location = new System.Drawing.Point(-1, 3);
+            this.lStats.Name = "lStats";
+            this.lStats.Size = new System.Drawing.Size(10, 13);
+            this.lStats.TabIndex = 3;
+            this.lStats.Text = "-";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // hSpeed
+            // 
+            this.hSpeed.Location = new System.Drawing.Point(130, 20);
+            this.hSpeed.Minimum = 1;
+            this.hSpeed.Name = "hSpeed";
+            this.hSpeed.Size = new System.Drawing.Size(85, 17);
+            this.hSpeed.TabIndex = 4;
+            this.hSpeed.Value = 50;
+            this.hSpeed.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hSpeed_Scroll);
+            // 
+            // cRun
+            // 
+            this.cRun.AutoSize = true;
+            this.cRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cRun.ForeColor = System.Drawing.Color.Red;
+            this.cRun.Location = new System.Drawing.Point(52, 22);
+            this.cRun.Name = "cRun";
+            this.cRun.Size = new System.Drawing.Size(75, 17);
+            this.cRun.TabIndex = 5;
+            this.cRun.Text = "Run (F2)";
+            this.cRun.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(52, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 20);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Step";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lFps
+            // 
+            this.lFps.AutoSize = true;
+            this.lFps.ForeColor = System.Drawing.Color.Red;
+            this.lFps.Location = new System.Drawing.Point(-1, 23);
+            this.lFps.Name = "lFps";
+            this.lFps.Size = new System.Drawing.Size(10, 13);
+            this.lFps.TabIndex = 7;
+            this.lFps.Text = "-";
+            // 
+            // FPS
+            // 
+            this.FPS.Enabled = true;
+            this.FPS.Interval = 1000;
+            this.FPS.Tick += new System.EventHandler(this.FPS_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(957, 594);
+            this.ControlBox = false;
+            this.Controls.Add(this.lFps);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cRun);
+            this.Controls.Add(this.hSpeed);
+            this.Controls.Add(this.lStats);
+            this.Controls.Add(this.bClear);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.pView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Game Of Life";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pView;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button bClear;
+        private System.Windows.Forms.Label lStats;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.HScrollBar hSpeed;
+        private System.Windows.Forms.CheckBox cRun;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lFps;
+        private System.Windows.Forms.Timer FPS;
+    }
+}
+
---
+++ GameOfLife/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Form1 : Form
+    {
+        SolidBrush cBrush = new SolidBrush(System.Drawing.Color.Red);
+        Rectangle cRect = new Rectangle(0, 0, 30, 30);
+        int CellsX = 300;
+        int CellsY = 150;
+
+        int cFPS = 0;
+
+        byte[,] Cells = new byte[300, 150];
+        byte[,] Cells2 = new byte[300, 150];
+
+
+        double CellsXsize = 0;
+        double CellsYsize = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            buttonExit.Left = this.Width - buttonExit.Width;
+            pView.Width = this.Width;
+            pView.Height = this.Height - pView.Top;
+
+            CellsXsize = (double)pView.Width / CellsX;
+            CellsYsize = (double)pView.Height / CellsY;
+
+            cRect.Width = Convert.ToInt32(CellsXsize);
+            cRect.Height = Convert.ToInt32(CellsYsize);
+
+        }
+
+        private void bClear_Click(object sender, EventArgs e)
+        {
+            ClearCells();
+            pView.Refresh();
+        }
+
+        private void pView_Paint(object sender, PaintEventArgs e)
+        {
+            for (int y = 0; y < CellsY; y++)
+            {
+                for (int x = 0; x < CellsX; x++)
+                {
+                    if (Cells[x,y] == 1)
+                    {
+                        double tempX = x * CellsXsize;
+                        double tempY = y * CellsYsize;
+                        cRect.X = Convert.ToInt32(tempX);
+                        cRect.Y = Convert.ToInt32(tempY);
+                        e.Graphics.FillRectangle(cBrush, cRect);
+                    }
+                }
+            }
+        }
+
+        private void pView_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pView_MouseDown(object sender, MouseEventArgs e)
+        {
+
+
+        }
+
+        private void pView_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+
+                    double selX = Math.Round((double)(e.X - CellsXsize / 2) / CellsXsize);
+                    double selY = Math.Round((double)(e.Y - CellsYsize / 2) / CellsYsize);
+
+                lStats.Text = Convert.ToString(selX) + ", " + Convert.ToString(selY);
+                   
+                if (e.Button == MouseButtons.Left)
+                {
+                    Cells[Convert.ToInt32(selX), Convert.ToInt32(selY)] = 1;
+                    pView.Refresh();
+                }
+                if (e.Button == MouseButtons.Right)
+                {
+                    Cells[Convert.ToInt32(selX), Convert.ToInt32(selY)] = 0;
+                    pView.Refresh();
+                }
+
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void hSpeed_Scroll(object sender, ScrollEventArgs e)
+        {
+            timer1.Interval = hSpeed.Value;
+        }
+
+        public void MainLoop()
+        {
+            for (int y = 0; y < CellsY; y++)
+            {
+                for (int x = 0; x < CellsX; x++)
+                {
+                    //Count neighbours
+                    byte cCount = 0;
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        int tempX = 0;
+                        int tempY = 0;
+
+                        switch (i)
+                        {
+                            case 0:
+                                tempX = mod((x - 1), CellsX);
+                                tempY = mod((y - 1), CellsY);
+                                break;
+                            case 1:
+                                tempX = mod(x, CellsX);
+                                tempY = mod((y - 1), CellsY);
+                                break;
+                            case 2:
+                                tempX = mod((x + 1),CellsX);
+                                tempY = mod((y - 1),CellsY);
+                                break;
+                            case 3:
+                                tempX = mod((x - 1),CellsX);
+                                tempY = mod(y, CellsY);
+                                break;
+                            case 4:
+                                tempX = mod((x + 1), CellsX);
+                                tempY = mod(y, CellsY);
+                                break;
+                            case 5:
+                                tempX = mod((x - 1), CellsX);
+                                tempY = mod((y + 1),CellsY);
+                                break;
+                            case 6:
+                                tempX = mod(x, CellsX);
+                                tempY = mod((y + 1), CellsY);
+                                break;
+                            case 7:
+                                tempX = mod((x + 1), CellsX);
+                                tempY = mod((y + 1),CellsY);
+                                break;
+                        }
+
+                        if (Cells[tempX,tempY] == 1)
+                            cCount++;
+                    }
+
+                    if (Cells[x, y] == 1) //Current cell is alive
+                    {
+                        if (cCount < 2) //Underpopulation, cell dies
+                            Cells2[x, y] = 0;
+                        if (cCount == 2 || cCount == 3) //Live on to next generation
+                            Cells2[x, y] = 1;
+                        if (cCount > 3) //Overpopulation, cell dies
+                            Cells2[x, y] = 0;
+                    }
+                    if (Cells[x, y] == 0) //Current cell is dead
+                    {
+                        if (cCount == 3) //A new cell is born!
+                            Cells2[x, y] = 1;
+                    }
+                }
+            }
+            CopyCellsFromBuffer();
+            cFPS++;
+        }
+
+        int mod(int x, int m)
+        {
+            return (x % m + m) % m;
+        }
+
+        public void CopyCellsFromBuffer()
+        {
+            for (int y = 0; y < CellsY; y++)
+            {
+                for (int x = 0; x < CellsX; x++)
+                {
+                    Cells[x, y] = Cells2[x, y];
+                }
+            }
+        }
+
+        public void ClearCells()
+        {
+            for (int y = 0; y < CellsY; y++)
+            {
+                for (int x = 0; x < CellsX; x++)
+                {
+                    Cells[x, y] = 0;
+                    Cells2[x, y] = 0;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (cRun.Checked == true)
+            {
+                MainLoop();
+                pView.Refresh();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainLoop();
+            pView.Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (Form.ModifierKeys == Keys.None && keyData == Keys.F2)
+            {
+
+                if (cRun.Checked == false)
+                    cRun.Checked = true;
+                else
+                    cRun.Checked = false;
+
+
+                return true;
+            }
+            return base.ProcessDialogKey(keyData);
+        }
+
+        private void FPS_Tick(object sender, EventArgs e)
+        {
+            lFps.Text = "FPS: " + cFPS.ToString();
+            cFPS = 0;
+        }
+    }
+}

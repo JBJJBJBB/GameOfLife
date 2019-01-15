43296b1aa1d548d981362dff2891ee135afdb5ec
Björn Dahlberg <Dallebull@hotmail.com>
2019-01-15 16:53:06 +0100

    Fixing and Trixing, cleaning Code. Known problem: Frames adds to the list and doesnt get changed from the Load position adn forward.

--- GameOfLife/DAL/Connection.cs
+++ GameOfLife/DAL/Connection.cs
@@ -1,26 +1,21 @@
-using System.Diagnostics;
-
 namespace GameOfLife
 {
-    using System;
     using System.Data.Entity;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Linq;
 
     public partial class Connection : DbContext
     {
         public Connection()
             : base("Context")
         {
-         //  Database.SetInitializer<Connection>(new DropCreateDatabaseAlways<Connection>()); //Debug or after DB change
+            //  Database.SetInitializer<Connection>(new DropCreateDatabaseAlways<Connection>()); //Debug or after DB change
             Database.SetInitializer<Connection>(new CreateDatabaseIfNotExists<Connection>());
-      
+
         }
 
-    
+
         public virtual DbSet<GameData> GameData { get; set; }
 
-  
+
         public virtual DbSet<FrameTable> FrameTables { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
